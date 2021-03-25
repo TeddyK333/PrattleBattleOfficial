122f07923d6922c5ccf4e819b2a9d1dffc3a644a
tetkata <80257820+tetkata@users.noreply.github.com>
2021-03-25 20:43:28 +0200

    new

---
+++ app/src/main/java/com/example/prattlebattle/LessonActivity.java
@@ -0,0 +1,19 @@
+package com.example.prattlebattle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LessonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson_activity);
+
+
+
+    }
+}
